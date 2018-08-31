edc4fa1cfaf63cc38fa1c1f1cdc056fc6923d722
tgrosh <tgrosh@yahoo.com>
2018-08-31 15:02:17 -0400

    Bouncy Block

---
+++ Assets/Scripts/Bouncy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncy : MonoBehaviour {
+    public float bounceForce;
+
+    private void Reset()
+    {
+        bounceForce = 1f;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody != null)
+        {
+            collision.rigidbody.AddForce(collision.contacts[0].normal * bounceForce, ForceMode.Impulse);
+        }
+    }
+}
