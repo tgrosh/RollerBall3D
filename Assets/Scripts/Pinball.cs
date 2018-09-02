a79a5f3bae94778662402eabcf61738db78299ef
tgrosh <tgrosh@yahoo.com>
2018-09-01 23:12:45 -0400

    Pinball Block

---
+++ Assets/Scripts/Pinball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pinball : MonoBehaviour {
+    public float pinballForce;
+
+    private bool pulling;
+    private float currentPull;
+    private Vector3 originalPosition;
+
+	// Use this for initialization
+	void Start () {
+        originalPosition = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        pulling = Input.GetMouseButton(0);
+
+        if (pulling)
+        {
+            currentPull += Time.deltaTime;
+            if (currentPull > 1)
+            {
+                currentPull = 1;
+            }
+        } else if (currentPull > 0)
+        {
+            currentPull = 0;
+        }
+
+        transform.localScale = new Vector3(2 - currentPull, transform.localScale.y, transform.localScale.z);
+        GetComponent<Rigidbody>().MovePosition(originalPosition - new Vector3(0, (currentPull / 2), 0));
+    }
+}
